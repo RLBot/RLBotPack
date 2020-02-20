3654cfe2ef0dcb2704f43e4c87b38aadf63a13c9
oxrock <oxrock2001@yahoo.com>
2020-02-19 22:11:24 -0500

    fixed aerials bug, setup script and updated config info

--- RLBotPack/Diablo/setup.py
+++ RLBotPack/Diablo/setup.py
@@ -1,15 +1,40 @@
-import urllib.request
 import zipfile
+import requests
 
 
-def downloadFile():
+
+
+def download_file_from_google_drive(id, destination):
     try:
-        urllib.request.urlretrieve("https://urlzs.com/XSvMA", 'rlutilities.zip')
+        URL = "https://docs.google.com/uc?export=download"
+
+        session = requests.Session()
+
+        response = session.get(URL, params = { 'id' : id }, stream = True)
+        token = get_confirm_token(response)
+
+        if token:
+            params = { 'id' : id, 'confirm' : token }
+            response = session.get(URL, params = params, stream = True)
+
+        save_response_content(response, destination)
     except Exception as e:
-        print("Error downloading file!")
-        print(e)
-        _ = input("Press enter to exit")
-        quit()
+        print(f"Download failed with error : {e}")
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
 
 def unzipFile():
     try:
@@ -25,9 +50,12 @@ def unzipFile():
 
 if __name__ == "__main__":
     print("Starting file download...")
-    downloadFile()
+    download_file_from_google_drive('1aXUUemEPm5XWFVr5WCzSDvl2IWjXue2v', 'rlutilities.zip')
     print("Download completed successfully.")
     print("Extracting file...")
     unzipFile()
     print("File extracted successfully.")
     print("Setup has completed successfully!")
+
+
+
