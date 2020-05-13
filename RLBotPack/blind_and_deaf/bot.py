188f698fd2fbd034c4d86065c2a5be88763b76c8
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-05-13 10:16:07 +0200

    Added "kickoffs" and fixed jumping into the air (#56)

--- RLBotPack/blind_and_deaf/bot.py
+++ RLBotPack/blind_and_deaf/bot.py
@@ -6,33 +6,44 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 from rlutilities.simulation import Game
-from rlutilities.linear_algebra import vec3
+from rlutilities.linear_algebra import vec3, norm
 
 from hover import Hover
+from kickoff import Kickoff
 
 HOVER_IDLE_HEIGHT = 300
 HOVER_IDLE_Y = 4800
 HOVER_MIN_HEIGHT = 140
-HOVER_MAX_HEIGHT = 1200
+HOVER_MAX_HEIGHT = 1000
 HOVER_MAX_SIDE = 1500
-HOVER_TARGET_Y = 4800
+HOVER_TARGET_Y = 4900
 
 
 def sign(x):
     return 1 if x >= 0 else -1
 
+def dist(a, b):
+    return norm(a - b)
+
 class MyBot(BaseAgent):
 
     def initialize_agent(self):
         # This runs once before the bot starts up
-        self.controller_state = SimpleControllerState()
+        self.controls = SimpleControllerState()
 
         self.info = Game(self.team)
-        self.hover = Hover(self.info.cars[self.index], self.info)
+        self.car = self.info.cars[self.index]
+        self.hover = Hover(self.car, self.info)
+        self.kickoff = Kickoff(self.car, self.info)
         self.sign = 2 * self.team - 1  # 1 if orange, else -1
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         self.info.read_game_information(packet, self.get_field_info())
+
+        if self.info.kickoff_pause and dist(self.info.ball.position, self.car.position) < 4000:
+            self.kickoff.step(self.info.time_delta)
+            self.controls = self.kickoff.controls
+            return self.controls
 
         ball_prediction = self.get_ball_prediction_struct()
         future_goal = self.find_future_goal(ball_prediction)
@@ -49,9 +60,9 @@ class MyBot(BaseAgent):
         # update controls
         self.hover.target = target
         self.hover.step(self.info.time_delta)
-        self.controller_state = self.hover.controls
+        self.controls = self.hover.controls
 
-        return self.controller_state
+        return self.controls
 
     def find_future_goal(self, ball_prediction):
         for step in ball_prediction.slices[:ball_prediction.num_slices]:
