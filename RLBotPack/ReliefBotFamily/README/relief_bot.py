0ec32203b8699039911fc41f2efb7fe4e2e8cf05
Tyler Arehart <tarehart@gmail.com>
2020-06-21 22:25:57 -0700

    ReliefBot now supports twitch interaction. AdversityBot better at heatseeker.

--- RLBotPack/ReliefBotFamily/README/relief_bot.py
+++ RLBotPack/ReliefBotFamily/README/relief_bot.py
@@ -1,6 +1,11 @@
+import os
+
 from rlbot.agents.base_agent import BOT_CONFIG_AGENT_HEADER
 from rlbot.agents.executable_with_socket_agent import ExecutableWithSocketAgent
+from rlbot.botmanager.helper_process_request import HelperProcessRequest
 from rlbot.parsing.custom_config import ConfigHeader, ConfigObject
+
+ACTION_SERVER_PORT = 4687
 
 
 class ReliefBot(ExecutableWithSocketAgent):
@@ -20,3 +25,10 @@ class ReliefBot(ExecutableWithSocketAgent):
         params = config.get_header(BOT_CONFIG_AGENT_HEADER)
         params.add_value('java_executable_path', str, default=None,
                          description='Relative path to the executable that runs java.')
+
+    def get_helper_process_request(self):
+        if self.is_executable_configured():
+            return HelperProcessRequest(python_file_path=None, key=__file__ + str(self.get_port()),
+                                        executable=self.executable_path, exe_args=[str(self.get_port()), f'--server.port={ACTION_SERVER_PORT}'],
+                                        current_working_directory=os.path.dirname(self.executable_path))
+        return None
