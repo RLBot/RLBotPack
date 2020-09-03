fc66d9d8f14da9d778aa2f6e482741645cf18a9f
Eric Veilleux <virx@virxcase.dev>
2020-09-02 20:38:18 -0400

    Update to COMM-77 (bug fixes & better 1's play) (#83)

--- RLBotPack/VirxEB/prediction.py
+++ RLBotPack/VirxEB/prediction.py
@@ -1,5 +1,6 @@
 import itertools
 import math
+import virxrlcu
 from threading import Event, Thread
 from traceback import print_exc
 
@@ -31,22 +32,82 @@ class Prediction(Thread):
                     foe_distances = tuple(self.agent.ball.location.flat_dist(foe.location) for foe in self.agent.foes if not foe.demolished)
                     self_dist = self.agent.ball.location.flat_dist(self.agent.me.location)
                     if len(foe_distances) > 0:
-                        shoot_threshold = 3840
+                        if self.agent.odd_tick == 0:
+                            cars = sorted((foe for foe in self.agent.foes if not foe.demolished), key=lambda foe: self.agent.ball.location.flat_dist(foe.location))
+                            found = False
 
-                        if len_friends == 1:
-                            shoot_threshold = 2560
-                        elif len_friends == 2:
-                            shoot_threshold = 1280
-                        elif len_friends > 2:
-                            shoot_threshold = 640
+                            for car in cars:
+                                me = (
+                                    car.location.tuple(),
+                                    car.forward.tuple(),
+                                    car.boost if self.agent.boost_amount != 'unlimited' else 100000,
+                                    car.local_velocity().x
+                                )
 
-                        if len_friends == 0:
-                            for i, foe_dist in enumerate(foe_distances):
-                                if foe_dist + 50 < self_dist and self.agent.ball_to_goal > shoot_threshold and self.agent.foes[i].location.y * side < self.agent.ball.location.y * side:
-                                    can_shoot = False
+                                game_info = (
+                                    self.agent.best_shot_value,
+                                    self.agent.boost_accel
+                                )
+
+                                me_a = (
+                                    me[0],
+                                    self.agent.me.velocity.tuple(),
+                                    self.agent.me.up.tuple(),
+                                    me[1],
+                                    1 if self.agent.me.airborne else -1,
+                                    me[2]
+                                )
+
+                                gravity = self.agent.gravity.tuple()
+
+                                ground = not car.airborne or car.location.z < 300
+
+                                # check every 12th slice
+                                for ball_slice in self.agent.ball_prediction_struct.slices[12::12]:
+                                    intercept_time = ball_slice.game_seconds
+                                    time_remaining = intercept_time - self.agent.time
+
+                                    if time_remaining <= 0:
+                                        break
+
+                                    ball_location = (ball_slice.physics.location.x, ball_slice.physics.location.y, ball_slice.physics.location.z)
+
+                                    if abs(ball_location[1]) > 5212:
+                                        break
+
+                                    if ground and ball_location[2] < 300:
+                                        shot = virxrlcu.parse_slice_for_jump_shot(time_remaining, *game_info, ball_location, *me)
+
+                                        if shot['found'] == 1:
+                                            self.agent.predictions['enemy_time_to_ball'] = intercept_time + 0.1
+                                            found = True
+                                            break
+                                    
+                                    if ground and ball_location[2] < 490 or ball_location[2] > 300:
+                                        shot = virxrlcu.parse_slice_for_double_jump(time_remaining, *game_info, ball_location, *me)
+
+                                        if shot['found'] == 1:
+                                            self.agent.predictions['enemy_time_to_ball'] = intercept_time + 0.1
+                                            found = True
+                                            break
+
+                                    if 500 < ball_location[2]:
+                                        shot = virxrlcu.parse_slice_for_aerial_shot(time_remaining, *game_info, gravity, ball_location, me_a)
+
+                                        if shot['found'] == 1:
+                                            self.agent.predictions['enemy_time_to_ball'] = intercept_time + 0.1
+                                            found = True
+                                            break
+                                
+                                if found:
+                                    break
+                            
+                            if not found:
+                                self.agent.predictions['enemy_time_to_ball'] = math.inf
 
                         self.agent.predictions['closest_enemy'] = min(foe_distances)
                     else:
+                        self.agent.predictions['enemy_time_to_ball'] = math.inf
                         self.agent.predictions['closest_enemy'] = math.inf
 
                 self.agent.predictions['self_from_goal'] = self.agent.friend_goal.location.flat_dist(self.agent.me.location) if not self.agent.me.demolished else math.inf
@@ -82,15 +143,15 @@ class Prediction(Thread):
                 else:
                     self.agent.playstyle = self.agent.playstyles.Neutral if self.agent.ball.location.y * side < 1280 else self.agent.playstyles.Defensive
 
-                if not can_shoot:
+                if not can_shoot and self.agent.can_shoot is None:
                     self.agent.can_shoot = self.agent.time - 2.95
 
                 if self.agent.odd_tick == 0:
                     is_own_goal = False
                     is_goal = False
 
-                    if self.agent.predictions['ball_struct'] is not None:
-                        for ball_slice in self.agent.predictions['ball_struct'].slices[30::6]:
+                    if self.agent.ball_prediction_struct is not None:
+                        for ball_slice in self.agent.ball_prediction_struct.slices[30::12]:
                             location = ball_slice.physics.location.y * side
 
                             if location >= 5212:
@@ -103,6 +164,9 @@ class Prediction(Thread):
 
                     if is_own_goal and not self.agent.predictions['own_goal']:
                         self.agent.send_quick_chat(QuickChats.CHAT_EVERYONE, QuickChats.Compliments_NiceShot)
+                    
+                    if is_goal and not self.agent.predictions['goal']:
+                        self.agent.send_quick_chat(QuickChats.CHAT_EVERYONE, QuickChats.Reactions_Wow)
 
                     self.agent.predictions["own_goal"] = is_own_goal
                     self.agent.predictions["goal"] = is_goal
