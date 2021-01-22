a10d64f58e743634fb0d0fc9184d790c15991484
L0laapk3 <L0laapk3@gmail.com>
2021-01-22 14:42:34 +0100

    shrunk puffy and laserboi rlu

--- RLBotPack/Laserboi/rlutilities/mechanics/__init__.pyi
+++ RLBotPack/Laserboi/rlutilities/mechanics/__init__.pyi
@@ -1,9 +1,5 @@
 import rlutilities.mechanics
-from typing import *
-from typing import Iterable as iterable
-from typing import Iterator as iterator
-from numpy import float64
-_Shape = Tuple[int, ...]
+import typing
 import rlutilities.simulation
 __all__  = [
 "Aerial",
@@ -24,57 +20,66 @@ class Aerial():
     @property
     def angle_threshold(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @angle_threshold.setter
     def angle_threshold(self, arg0: float) -> None:
         pass
     @property
     def arrival_time(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @arrival_time.setter
     def arrival_time(self, arg0: float) -> None:
         pass
     @property
     def controls(self) -> rlutilities.simulation.Input:
         """
-:type: rlutilities.simulation.Input"""
+        :type: rlutilities.simulation.Input
+        """
     @property
     def finished(self) -> bool:
         """
-:type: bool"""
+        :type: bool
+        """
     @property
     def reorient_distance(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @reorient_distance.setter
     def reorient_distance(self, arg0: float) -> None:
         pass
     @property
     def target_orientation(self) -> mat<3,3>:
         """
-:type: mat<3,3>"""
+        :type: mat<3,3>
+        """
     @target_orientation.setter
     def target_orientation(self, arg0: mat<3,3>) -> None:
         pass
     @property
     def target_position(self) -> vec<3>:
         """
-:type: vec<3>"""
+        :type: vec<3>
+        """
     @target_position.setter
     def target_position(self, arg0: vec<3>) -> None:
         pass
     @property
     def throttle_distance(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @throttle_distance.setter
     def throttle_distance(self, arg0: float) -> None:
         pass
     @property
     def up(self) -> vec<3>:
         """
-:type: vec<3>"""
+        :type: vec<3>
+        """
     @up.setter
     def up(self, arg0: vec<3>) -> None:
         pass
@@ -89,11 +94,13 @@ class Boostdash():
     @property
     def controls(self) -> rlutilities.simulation.Input:
         """
-:type: rlutilities.simulation.Input"""
+        :type: rlutilities.simulation.Input
+        """
     @property
     def finished(self) -> bool:
         """
-:type: bool"""
+        :type: bool
+        """
     pass
 class Dodge():
     def __init__(self, arg0: rlutilities.simulation.Car) -> None: ...
@@ -102,43 +109,50 @@ class Dodge():
     @property
     def controls(self) -> rlutilities.simulation.Input:
         """
-:type: rlutilities.simulation.Input"""
+        :type: rlutilities.simulation.Input
+        """
     @property
     def delay(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @delay.setter
     def delay(self, arg0: float) -> None:
         pass
     @property
     def direction(self) -> vec<2>:
         """
-:type: vec<2>"""
+        :type: vec<2>
+        """
     @direction.setter
     def direction(self, arg0: vec<2>) -> None:
         pass
     @property
     def finished(self) -> bool:
         """
-:type: bool"""
+        :type: bool
+        """
     @property
     def jump_duration(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @jump_duration.setter
     def jump_duration(self, arg0: float) -> None:
         pass
     @property
     def preorientation(self) -> mat<3,3>:
         """
-:type: mat<3,3>"""
+        :type: mat<3,3>
+        """
     @preorientation.setter
     def preorientation(self, arg0: mat<3,3>) -> None:
         pass
     @property
     def timer(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     forward_torque = 224.0
     input_threshold = 0.5
     side_torque = 260.0
@@ -160,35 +174,40 @@ class Drive():
     @property
     def controls(self) -> rlutilities.simulation.Input:
         """
-:type: rlutilities.simulation.Input"""
+        :type: rlutilities.simulation.Input
+        """
     @controls.setter
     def controls(self, arg0: rlutilities.simulation.Input) -> None:
         pass
     @property
     def finished(self) -> bool:
         """
-:type: bool"""
+        :type: bool
+        """
     @finished.setter
     def finished(self, arg0: bool) -> None:
         pass
     @property
     def reaction_time(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @reaction_time.setter
     def reaction_time(self, arg0: float) -> None:
         pass
     @property
     def speed(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @speed.setter
     def speed(self, arg0: float) -> None:
         pass
     @property
     def target(self) -> vec<3>:
         """
-:type: vec<3>"""
+        :type: vec<3>
+        """
     @target.setter
     def target(self, arg0: vec<3>) -> None:
         pass
@@ -204,29 +223,34 @@ class FollowPath():
     @property
     def arrival_speed(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @arrival_speed.setter
     def arrival_speed(self, arg0: float) -> None:
         pass
     @property
     def arrival_time(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @arrival_time.setter
     def arrival_time(self, arg0: float) -> None:
         pass
     @property
     def controls(self) -> rlutilities.simulation.Input:
         """
-:type: rlutilities.simulation.Input"""
+        :type: rlutilities.simulation.Input
+        """
     @property
     def finished(self) -> bool:
         """
-:type: bool"""
+        :type: bool
+        """
     @property
     def path(self) -> rlutilities.simulation.Curve:
         """
-:type: rlutilities.simulation.Curve"""
+        :type: rlutilities.simulation.Curve
+        """
     @path.setter
     def path(self, arg0: rlutilities.simulation.Curve) -> None:
         pass
@@ -238,21 +262,24 @@ class Jump():
     @property
     def controls(self) -> rlutilities.simulation.Input:
         """
-:type: rlutilities.simulation.Input"""
+        :type: rlutilities.simulation.Input
+        """
     @controls.setter
     def controls(self, arg0: rlutilities.simulation.Input) -> None:
         pass
     @property
     def duration(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @duration.setter
     def duration(self, arg0: float) -> None:
         pass
     @property
     def finished(self) -> bool:
         """
-:type: bool"""
+        :type: bool
+        """
     @finished.setter
     def finished(self, arg0: bool) -> None:
         pass
@@ -268,46 +295,53 @@ class Reorient():
     @property
     def alpha(self) -> vec<3>:
         """
-:type: vec<3>"""
+        :type: vec<3>
+        """
     @property
     def controls(self) -> rlutilities.simulation.Input:
         """
-:type: rlutilities.simulation.Input"""
+        :type: rlutilities.simulation.Input
+        """
     @controls.setter
     def controls(self, arg0: rlutilities.simulation.Input) -> None:
         pass
     @property
     def eps_omega(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @eps_omega.setter
     def eps_omega(self, arg0: float) -> None:
         pass
     @property
     def eps_phi(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @eps_phi.setter
     def eps_phi(self, arg0: float) -> None:
         pass
     @property
     def finished(self) -> bool:
         """
-:type: bool"""
+        :type: bool
+        """
     @finished.setter
     def finished(self, arg0: bool) -> None:
         pass
     @property
     def horizon_time(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @horizon_time.setter
     def horizon_time(self, arg0: float) -> None:
         pass
     @property
     def target_orientation(self) -> mat<3,3>:
         """
-:type: mat<3,3>"""
+        :type: mat<3,3>
+        """
     @target_orientation.setter
     def target_orientation(self, arg0: mat<3,3>) -> None:
         pass
@@ -319,28 +353,32 @@ class ReorientML():
     @property
     def controls(self) -> rlutilities.simulation.Input:
         """
-:type: rlutilities.simulation.Input"""
+        :type: rlutilities.simulation.Input
+        """
     @controls.setter
     def controls(self, arg0: rlutilities.simulation.Input) -> None:
         pass
     @property
     def eps_phi(self) -> float:
         """
-:type: float"""
+        :type: float
+        """
     @eps_phi.setter
     def eps_phi(self, arg0: float) -> None:
         pass
     @property
     def finished(self) -> bool:
         """
-:type: bool"""
+        :type: bool
+        """
     @finished.setter
     def finished(self, arg0: bool) -> None:
         pass
     @property
     def target_orientation(self) -> mat<3,3>:
         """
-:type: mat<3,3>"""
+        :type: mat<3,3>
+        """
     @target_orientation.setter
     def target_orientation(self, arg0: mat<3,3>) -> None:
         pass
@@ -351,16 +389,19 @@ class Wavedash():
     @property
     def controls(self) -> rlutilities.simulation.Input:
         """
-:type: rlutilities.simulation.Input"""
+        :type: rlutilities.simulation.Input
+        """
     @property
     def direction(self) -> vec<2>:
         """
-:type: vec<2>"""
+        :type: vec<2>
+        """
     @direction.setter
     def direction(self, arg0: vec<2>) -> None:
         pass
     @property
     def finished(self) -> bool:
         """
-:type: bool"""
+        :type: bool
+        """
     pass
