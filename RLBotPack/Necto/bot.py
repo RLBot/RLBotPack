132beb1595ed1360cca57577c998858ca617b1a6
Rolv-Arild <rolv_arild@hotmail.com>
2022-02-25 21:17:28 +0100

    Add kickoffs and deterministic actions to Necto

--- RLBotPack/Necto/bot.py
+++ RLBotPack/Necto/bot.py
@@ -1,6 +1,5 @@
-import time
-
 import numpy as np
+import torch
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 from rlgym_compat import GameState
@@ -8,14 +7,33 @@ from rlgym_compat import GameState
 from agent import Agent
 from necto_obs import NectoObsBuilder
 
+KICKOFF_CONTROLS = (
+        11 * 4 * [SimpleControllerState(throttle=1, boost=True)]
+        + 4 * 4 * [SimpleControllerState(throttle=1, boost=True, steer=-1)]
+        + 2 * 4 * [SimpleControllerState(throttle=1, jump=True, boost=True)]
+        + 1 * 4 * [SimpleControllerState(throttle=1, boost=True)]
+        + 1 * 4 * [SimpleControllerState(throttle=1, yaw=0.8, pitch=-0.7, jump=True, boost=True)]
+        + 13 * 4 * [SimpleControllerState(throttle=1, pitch=1, boost=True)]
+        + 10 * 4 * [SimpleControllerState(throttle=1, roll=1, pitch=0.5)]
+)
+
+KICKOFF_NUMPY = np.array([
+    [scs.throttle, scs.steer, scs.pitch, scs.yaw, scs.roll, scs.jump, scs.boost, scs.handbrake]
+    for scs in KICKOFF_CONTROLS
+])
+
 
 class Necto(BaseAgent):
-    def __init__(self, name, team, index):
+    def __init__(self, name, team, index, beta=1):
         super().__init__(name, team, index)
 
-        self.obs_builder = NectoObsBuilder()
+        self.obs_builder = None
         self.agent = Agent()
         self.tick_skip = 8
+
+        # Beta controls randomness:
+        # 1=best action, 0.5=sampling from probability, 0=random, -1=worst action, or anywhere inbetween
+        self.beta = beta
 
         self.game_state: GameState = None
         self.controls = None
@@ -23,45 +41,109 @@ class Necto(BaseAgent):
         self.update_action = True
         self.ticks = 0
         self.prev_time = 0
-        print('RLGymExampleBot Ready - Index:', index)
+        self.kickoff_index = -1
+        print('Necto Ready - Index:', index)
 
     def initialize_agent(self):
         # Initialize the rlgym GameState object now that the game is active and the info is available
-        self.game_state = GameState(self.get_field_info())
+        field_info = self.get_field_info()
+        self.obs_builder = NectoObsBuilder(field_info=field_info)
+        self.game_state = GameState(field_info)
         self.ticks = self.tick_skip  # So we take an action the first tick
         self.prev_time = 0
         self.controls = SimpleControllerState()
         self.action = np.zeros(8)
         self.update_action = True
+        self.kickoff_index = -1
+
+    def render_attention_weights(self, weights, obs):
+        mean_weights = torch.mean(torch.stack(weights), dim=0).numpy()[0][0]
+
+        top = sorted(range(len(mean_weights)), key=lambda i: mean_weights[i], reverse=True)
+        top.remove(1)  # Self
+
+        self.renderer.begin_rendering('attention_weights')
+
+        invert = np.array([-1, -1, 1]) if self.team == 1 else np.ones(3)
+        loc = obs[0][0, 0, 5:8] * 2300 * invert
+        mx = mean_weights[~(np.arange(len(mean_weights)) == 1)].max()
+        c = 1
+        for i in top[:3]:
+            weight = mean_weights[i] / mx
+            # print(i, weight)
+            dest = loc + obs[1][0, i, 5:8] * 2300 * invert
+            color = self.renderer.create_color(255, round(255 * (1 - weight)), round(255),
+                                               round(255 * (1 - weight)))
+            self.renderer.draw_string_3d(dest, 2, 2, str(c), color)
+            c += 1
+            self.renderer.draw_line_3d(loc, dest, color)
+        self.renderer.end_rendering()
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        cur_time = time.perf_counter()
+        # THINGS TO TEST:
+        # Deterministic
+        # Invert boosts
+        # If 2 tick delay works well, try 3 tick etc.
+        cur_time = packet.game_info.seconds_elapsed
         delta = cur_time - self.prev_time
         self.prev_time = cur_time
 
-        ticks_elapsed = delta * 120  # Smaller than 1/120 on purpose
+        ticks_elapsed = delta * 120
         self.ticks += ticks_elapsed
         self.game_state.decode(packet, ticks_elapsed)
 
-        if self.update_action and len(self.game_state.players) > self.index:
-            self.update_action = False
+        if self.update_action == 1 and len(self.game_state.players) > self.index:
+            self.update_action = 0
 
             player = self.game_state.players[self.index]
             teammates = [p for p in self.game_state.players if p.team_num == self.team and p != player]
             opponents = [p for p in self.game_state.players if p.team_num != self.team]
 
-            # TODO Maybe draw some stuff based on attention scores?
-            # self.renderer.draw_string_3d(closest_op.car_data.position, 2, 2, "CLOSEST", self.renderer.white())
-
             self.game_state.players = [player] + teammates + opponents
 
             obs = self.obs_builder.build_obs(player, self.game_state, self.action)
-            self.action = self.agent.act(obs)
+            self.action, weights = self.agent.act(obs, self.beta)
+
+            self.render_attention_weights(weights, obs)
+
+        # self.update_action -= 1
 
         if self.ticks >= self.tick_skip:
             self.ticks = 0
             self.update_controls(self.action)
-            self.update_action = True
+            self.update_action = 1
+
+        if packet.game_info.is_kickoff_pause:
+            if self.kickoff_index == -1:
+                is_kickoff_taker = False
+                ball_pos = np.array([packet.game_ball.physics.location.x, packet.game_ball.physics.location.y])
+                positions = np.array([[car.physics.location.x, car.physics.location.y]
+                                      for car in packet.game_cars[:packet.num_cars]])
+                distances = np.linalg.norm(positions - ball_pos, axis=1)
+                if abs(distances.min() - distances[self.index]) <= 10:
+                    is_kickoff_taker = True
+                    indices = np.argsort(distances)
+                    for index in indices:
+                        if abs(distances[index] - distances[self.index]) <= 10 \
+                                and packet.game_cars[index].team == self.team \
+                                and index != self.index:
+                            if self.team == 0:
+                                is_left = positions[index, 0] < positions[self.index, 0]
+                            else:
+                                is_left = positions[index, 0] > positions[self.index, 0]
+                            if not is_left:
+                                is_kickoff_taker = False  # Left goes
+
+                self.kickoff_index = 0 if is_kickoff_taker else -2
+
+            if 0 <= self.kickoff_index < len(KICKOFF_NUMPY) \
+                    and packet.game_ball.physics.location.y == 0:
+                action = KICKOFF_NUMPY[self.kickoff_index]
+                self.action = action
+                self.update_controls(self.action)
+                self.kickoff_index += 1
+        else:
+            self.kickoff_index = -1
 
         return self.controls
 
