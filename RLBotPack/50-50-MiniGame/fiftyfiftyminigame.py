e45f5638dcf42322bc11d9f99d3d8de0d23b078e
Dan (Smash Repair) <46916734+Dan-SmashRepair@users.noreply.github.com>
2022-03-09 18:24:01 +1100

    Update fiftyfiftyminigame.py

--- RLBotPack/50-50-MiniGame/fiftyfiftyminigame.py
+++ RLBotPack/50-50-MiniGame/fiftyfiftyminigame.py
@@ -5,9 +5,13 @@ from rlbot.agents.base_script import BaseScript
 from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3, Rotator, GameInfoState
 
 
-# Extending the BaseScript class is purely optional. It's just convenient / abstracts you away from
-# some strange classes like GameInterface
 class FiftyFiftyMiniGame(BaseScript):
+    '''
+    FiftyFiftyMiniGame is a RLBot script
+    the goal it to be the first to touch the ball outside the center circle
+    it is designed for 1v1 and works best with the 'disable goal reset' mutator active
+    both players spawn in the center circle with a slightly randomized rotation
+    '''
     def __init__(self):
         super().__init__("FiftyFiftyMiniGame")
         self.game_phase = 0
@@ -15,6 +19,8 @@ class FiftyFiftyMiniGame(BaseScript):
         self.scoreDiff_prev = 0
         self.prev_ticks = 0
         self.ticks = 0
+        self.disable_goal_reset = False
+        self.pause_ticks = 40
 
 
     def run(self):
@@ -26,20 +32,33 @@ class FiftyFiftyMiniGame(BaseScript):
             packet = self.get_game_tick_packet()
             self.ticks += 1
 
-            # phase 0: setup round
+            # check if 'disable goal reset' mutator is active
+            if self.ticks == 1:
+                match_settings = self.get_match_settings()
+                mutators = match_settings.MutatorSettings()
+                self.disable_goal_reset = True if mutators.RespawnTimeOption() == 3 else None
+
+            '''phase 0''' # setup round
             if self.game_phase == 0 and packet.game_info.is_kickoff_pause:
                 self.setup_newround(packet)
+
+            # same as above, but custom built for 'disable goal reset' mutator active
+            if self.disable_goal_reset == True:
+                self.setup_newround_DGR(packet)
+
+            # pause for 'pause_ticks' then resume
+            if self.game_phase == -1 and self.ticks - self.prev_ticks == 2: # glitch-fix: wait a couple of ticks before pause
+                self.set_game_state(GameState(game_info=GameInfoState(paused=True)))
+            elif self.game_phase == -1 and self.ticks - self.prev_ticks == self.pause_ticks:
+                self.set_game_state(GameState(game_info=GameInfoState(paused=False)))
                 self.game_phase = 1
 
-            # same as above, but custom built for 'disable goal reset'
-            self.setup_newround_DGR(packet)
-
-            # phase 1: ball remains in circle
+            '''phase 1''' # ball remains in circle
             if self.game_phase == 1 and np.linalg.norm(np.array([packet.game_ball.physics.location.x, packet.game_ball.physics.location.y, packet.game_ball.physics.location.z]) - np.array([0, 0, 97])) > 1200:
                 phase2_time = packet.game_info.seconds_elapsed
                 self.game_phase = 2
 
-            # phase 2: after ball has left circle, next touch wins. Increment score and reset phase
+            '''phase 2''' # after ball leaves circle, next touch wins. Increment score and reset phase
             if self.game_phase == 2 and packet.game_ball.latest_touch.time_seconds > phase2_time:
                 if packet.game_ball.latest_touch.team == 0:
                     ball_y_side = 1
@@ -49,47 +68,59 @@ class FiftyFiftyMiniGame(BaseScript):
                 self.set_game_state(GameState(ball=ball_state))
                 self.game_phase = 0
             
-            # phase 2(special case): when goal scored with no further touches, increment score and reset phase
+            # phase 2(special case): when goal scored with no further touches, reset phase
             if self.game_phase == 2 and packet.game_info.is_kickoff_pause and packet.game_ball.latest_touch.time_seconds <= phase2_time:
                 self.game_phase = 0
-
+            
 
     def setup_newround_DGR(self, packet):
         # check if goal in last tick
         teamScores = tuple(map(lambda x: x.score, packet.teams))
         scoreDiff = max(teamScores) - min(teamScores)
 
-        # setup round and pause
-        if self.game_phase == 0 and scoreDiff != self.scoreDiff_prev:                  
+        # setup round
+        if scoreDiff != self.scoreDiff_prev:
             self.setup_newround(packet)
             self.scoreDiff_prev = scoreDiff
-            self.prev_ticks = self.ticks
-            self.game_phase = -1
-            self.set_game_state(GameState(game_info=GameInfoState(paused=True)))
-            
-        # wait then resume
-        if self.game_phase == -1 and self.ticks - self.prev_ticks == 60:
-            self.set_game_state(GameState(game_info=GameInfoState(paused=False)))
-            self.game_phase = 1
 
 
     def setup_newround(self, packet):
         car_states = {}
+        yaw, yaw_mir = self.yaw_randomizor()
         for p in range(packet.num_cars):
             car = packet.game_cars[p]
             if car.team == 0:
-                yaw = math.pi / 2.0
                 car_state = CarState(boost_amount=100, physics=Physics(location=Vector3(0, -1000, 17), rotation=Rotator(yaw=yaw, pitch=0, roll=0), velocity=Vector3(0, 0, 0),
                         angular_velocity=Vector3(0, 0, 0)))
                 car_states[p] = car_state
             elif car.team == 1:
-                yaw = math.pi / -2.0
-                car_state = CarState(boost_amount=100, physics=Physics(location=Vector3(0, 1000, 17), rotation=Rotator(yaw=yaw, pitch=0, roll=0), velocity=Vector3(0, 0, 0),
+                car_state = CarState(boost_amount=100, physics=Physics(location=Vector3(0, 1000, 17), rotation=Rotator(yaw=-yaw_mir, pitch=0, roll=0), velocity=Vector3(0, 0, 0),
                         angular_velocity=Vector3(0, 0, 0)))
                 car_states[p] = car_state
-        ball_state = BallState(Physics(location=Vector3(0, 0, 97)))
+        ball_state = BallState(Physics(location=Vector3(0, 0, 93)))
         game_state = GameState(ball=ball_state, cars=car_states)
         self.set_game_state(game_state)
+        self.prev_ticks = self.ticks
+        self.game_phase = -1
+
+
+    def yaw_randomizor(self):
+        if self.ticks > 1000:
+            # yaw will have 5 possible values from pi*.25 to pi.75, using ticks as random value. Straght kickoffs weighted higher at 50% chance
+            if self.ticks % 8 - 1 <= 1:
+                yaw = math.pi * 0.5
+            else:
+                yaw = math.pi * (self.ticks % 8 - 1) * 0.125
+            # 50% parallel/mirrored yaw compared to other team
+            if self.ticks % 16 >= 8:
+                yaw_mir = math.pi-yaw
+            else:
+                yaw_mir = yaw
+            return yaw, yaw_mir
+        else:
+            yaw_mir = yaw = math.pi * 0.5
+            return yaw, yaw_mir
+
 
 # You can use this __name__ == '__main__' thing to ensure that the script doesn't start accidentally if you
 # merely reference its module from somewhere
