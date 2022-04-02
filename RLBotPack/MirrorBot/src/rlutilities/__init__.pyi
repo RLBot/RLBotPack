b185edb6ea1e3e688d15fb68c0ab6f7330119186
Jiří Pihrt <jira.pihrt@volny.cz>
2022-04-02 13:44:12 +0200

    Add Mirror Bot

---
+++ RLBotPack/MirrorBot/src/rlutilities/__init__.pyi
@@ -0,0 +1,13 @@
+from typing import *
+
+_Shape = Tuple[int, ...]
+__all__ = [
+    "initialize",
+    "linear_algebra",
+    "mechanics",
+    "simulation"
+]
+
+
+def initialize(arg0: str) -> None:
+    pass
