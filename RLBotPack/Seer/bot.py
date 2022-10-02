61dcb6400f7812973a6d8a522f2a6e67508948be
Neville Walo <neville.walo@gmail.com>
2022-10-02 12:31:50 +0200

    added Seerv2 skeleton

--- RLBotPack/Seer/bot.py
+++ RLBotPack/Seer/bot.py
@@ -1,10 +1,59 @@
-import numpy as np
-import torch
-from numpy import ndarray
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
+from SeerPPO import SeerNetwork, impute_features
+import torch
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from numba import jit
+from numpy import ndarray
+import math
 
-from helper import invert_player_data, invert_ball_data, invert_boost_data, Seer_Network, flip_timeout, get_action_encoding, impute_features
+enc = OneHotEncoder(sparse=False, drop='if_binary',
+                    categories=[np.array([0., 1., 2.]), np.array([0., 1., 2., 3., 4.]), np.array([0., 1., 2., 3., 4.]), np.array([0., 1., 2.]), np.array([0., 1.]), np.array([0., 1.]),
+                                np.array([0., 1.])])
+
+
+def get_action_encoding(y_train: np.ndarray):
+    assert y_train.shape[1] == 7
+
+    result = enc.fit_transform(y_train)
+
+    assert result.shape[1] == 19
+
+    return result
+
+
+@jit(nopython=True, fastmath=True)
+def invert_player_data(player_data: np.ndarray) -> np.ndarray:
+    assert len(player_data) == 16
+    player_data = player_data * np.array([-1, -1, 1, 1, 1, 1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1], dtype=np.float32)
+    player_data[4] = invert_yaw(player_data[4])
+    return player_data
+
+
+@jit(nopython=True, fastmath=True)
+def invert_ball_data(ball_data: np.ndarray) -> np.ndarray:
+    assert len(ball_data) == 9
+    ball_data = ball_data * np.array([-1, -1, 1, -1, -1, 1, -1, -1, 1], dtype=np.float32)
+    return ball_data
+
+
+@jit(nopython=True, fastmath=True)
+def invert_boost_data(boost_data: np.ndarray) -> np.ndarray:
+    assert boost_data.shape[0] == 34
+    # just the inverse
+    return boost_data[::-1]
+
+
+@jit(nopython=True, fastmath=True)
+def invert_yaw(yaw):
+    tol = 1e-4
+    assert -math.pi - tol <= yaw <= math.pi + tol
+    yaw += math.pi  # yaw in [- pi, pi]
+    if yaw > math.pi:
+        yaw -= 2 * math.pi
+    assert -math.pi - tol <= yaw <= math.pi + tol
+    return yaw
 
 
 def encode_player(packet: GameTickPacket, index: int, has_flip: bool, demo_timer: float, inverted: bool) -> ndarray:
@@ -66,11 +115,12 @@ def encode_boost(packet: GameTickPacket, inverted: bool) -> ndarray:
     return array
 
 
-class Seer(BaseAgent):
+class MyBot(BaseAgent):
 
     def __init__(self, name, team, index):
-        print("Seer Loading...")
         super().__init__(name, team, index)
+
+        self.game_over = False
         self.tick_skip = 8
         self.ticks = 8  # # So we take an action the first tick
         self.prev_time = 0
@@ -82,13 +132,13 @@ class Seer(BaseAgent):
             self.inverted = True
             self.opponent_index = 0
 
-        self.device = 'cpu'
-        self.filename = "./Seer/Seer.pt"
+        self.filename = "./Seer/13250.pt"
 
-        self.model = Seer_Network()
-        state_dict = torch.load(self.filename, map_location=torch.device(self.device))
-        self.model.load_state_dict(state_dict)
-        self.model.eval()
+        print("Seer Loading: " + self.filename)
+
+        self.policy = SeerNetwork()
+        self.policy.load_state_dict(torch.load(self.filename))
+        self.policy.eval()
 
         self.controls = SimpleControllerState()
         self.agent_latest_wheel_contact: float = 0.0
@@ -99,16 +149,19 @@ class Seer(BaseAgent):
         self.last_packet_time_demo = 0.0
 
         self.prev_action = None
-
-        self.state = None
+        self.lstm_states = None
+        self.episode_starts = torch.zeros(1, dtype=torch.float32, requires_grad=False)
         self.reset_states()
-        print("Seer Ready!")
+
+        self.compiled = False
+
+        print("Seer Ready: " + self.filename)
 
     def initialize_agent(self):
         pass
 
     def reset_states(self):
-        self.state = (torch.zeros(1, 1, 512, device=self.device, requires_grad=False), torch.zeros(1, 1, 512, device=self.device, requires_grad=False))
+        self.lstm_states = (torch.zeros(1, 1, self.policy.LSTM.hidden_size, requires_grad=False), torch.zeros(1, 1, self.policy.LSTM.hidden_size, requires_grad=False))
         self.prev_action = np.array([1.0, 2.0, 2.0, 1.0, 0.0, 0.0, 0.0], dtype=np.float32)
 
     def get_flips(self, packet: GameTickPacket):
@@ -119,8 +172,8 @@ class Seer(BaseAgent):
         if packet.game_cars[self.opponent_index].has_wheel_contact:
             self.opponent_latest_wheel_contact = packet.game_info.seconds_elapsed
 
-        agent_flip_timeout = (packet.game_info.seconds_elapsed - self.agent_latest_wheel_contact) > flip_timeout and packet.game_cars[self.index].jumped
-        opponent_flip_timeout = (packet.game_info.seconds_elapsed - self.opponent_latest_wheel_contact) > flip_timeout and packet.game_cars[self.opponent_index].jumped
+        agent_flip_timeout = (packet.game_info.seconds_elapsed - self.agent_latest_wheel_contact) > 1.5 and packet.game_cars[self.index].jumped
+        opponent_flip_timeout = (packet.game_info.seconds_elapsed - self.opponent_latest_wheel_contact) > 1.5 and packet.game_cars[self.opponent_index].jumped
 
         agent_flip = not packet.game_cars[self.index].double_jumped and not agent_flip_timeout
         opponent_flip = not packet.game_cars[self.opponent_index].double_jumped and not opponent_flip_timeout
@@ -154,7 +207,7 @@ class Seer(BaseAgent):
         ticks_elapsed = delta * 120
         self.ticks += ticks_elapsed
 
-        if not packet.game_info.is_round_active:
+        if not packet.game_info.is_round_active and self.compiled:
             self.controls.throttle = 0.0
             self.controls.steer = 0.0
             self.controls.yaw = 0.0
@@ -166,43 +219,38 @@ class Seer(BaseAgent):
             self.reset_states()
             return self.controls
 
-        if self.ticks >= self.tick_skip:
+        elif self.ticks >= self.tick_skip:
             self.ticks = 0
             self.update_controls(packet)
 
         return self.controls
 
-    def update_controls(self, packet: GameTickPacket):
-
+    def build_obs(self, packet):
         agent_flip, opponent_flip = self.get_flips(packet)
         agent_demo_timer, opponent_demo_timer = self.get_demo_timers(packet)
-        input_array = self.packet_to_array(packet, self.index, self.opponent_index, agent_flip, opponent_flip, agent_demo_timer, opponent_demo_timer, self.inverted)
-
-        with torch.no_grad():
-            input_tensor = torch.tensor(input_array.reshape((1, -1)), dtype=torch.float32, device=self.device, requires_grad=False)
-            episode_starts = torch.zeros(1, dtype=torch.float32, device=self.device, requires_grad=False)
-            actions, values, log_prob, self.state = self.model(input_tensor, self.state, episode_starts, True)
-            actions = actions.numpy()
-
-        self.update_controller_from_action(actions)
-
-    def packet_to_array(self, packet: GameTickPacket, agent_index: int, opponent_index: int, agent_has_flip: bool, opponent_has_flip: bool, agent_demo_timer: float, opponent_demo_timer: float,
-                        inverted: bool) -> ndarray:
-        player_0 = encode_player(packet, agent_index, agent_has_flip, agent_demo_timer, inverted)
-        player_1 = encode_player(packet, opponent_index, opponent_has_flip, opponent_demo_timer, inverted)
-        boost = encode_boost(packet, inverted)
-        ball = encode_ball(packet, inverted)
+        player_0 = encode_player(packet, self.index, agent_flip, agent_demo_timer, self.inverted)
+        player_1 = encode_player(packet, self.opponent_index, opponent_flip, opponent_demo_timer, self.inverted)
+        boost = encode_boost(packet, self.inverted)
+        ball = encode_ball(packet, self.inverted)
 
         prev_action_encoding = get_action_encoding(self.prev_action.reshape(1, -1))
+        obs = impute_features(player_0, player_1, boost, ball, prev_action_encoding.reshape(-1)).reshape(1, -1)
+        obs = torch.tensor(obs, dtype=torch.float32)
+        self.compiled = True
 
-        x_train = impute_features(player_0, player_1, boost, ball, prev_action_encoding.reshape(-1)).reshape(1, -1)
+        return obs
 
-        return x_train
+    def update_controls(self, packet: GameTickPacket):
+
+        with torch.no_grad():
+            obs = self.build_obs(packet)
+            action, self.lstm_states = self.policy.predict_actions(obs, self.lstm_states, self.episode_starts, True)
+            action = action.numpy()[0]
+            self.prev_action = action
+
+        self.update_controller_from_action(action)
 
     def update_controller_from_action(self, actions: ndarray):
-        actions = actions[0]
-
-        self.prev_action = actions
 
         steer_yaw = actions[1] * 0.5 - 1.0
 
