cf8adeed9941cfb0efb67414ad57b03711ec598f
Neville Walo <neville.walo@gmail.com>
2022-11-05 18:38:24 +0100

    added v1

--- RLBotPack/SeerV1/bot.py
+++ RLBotPack/SeerV1/bot.py
@@ -1,14 +1,6 @@
-from SeerPPO import SeerBot, Agent
-from SeerPPO.V1 import SeerActionV1
-from SeerPPO.V1 import SeerObsV1
-from SeerPPO.V1 import SeerNetworkV1
+from SeerPPO.V1 import SeerV1Template
 
 
-class SeerV1(SeerBot):
+class SeerV1(SeerV1Template):
     def __init__(self, name, team, index):
-        super().__init__(name, team, index)
-
-        self.obs_builder = SeerObsV1()
-        self.act_parser = SeerActionV1()
-        self.agent = Agent("./SeerV1/SeerV1_30000.pt", SeerNetworkV1())
-        self.name = "SeerV1"
+        super().__init__(name, team, index, "./SeerV1/SeerV1_30000.pt")
