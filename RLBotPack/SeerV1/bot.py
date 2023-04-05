3805680d4728370466d32ca32fdba067038c6d90
Darxeal <jira.pihrt@volny.cz>
2023-04-05 15:33:22 +0200

    Revert "Updated Seer (#270)"
    This reverts commit f44944f19dace15b3caa96146a70406e6e71ba3b.

--- RLBotPack/SeerV1/bot.py
+++
@@ -1,6 +0,0 @@
-from SeerPPO.V1 import SeerV1Template
-
-
-class SeerV1(SeerV1Template):
-    def __init__(self, name, team, index):
-        super().__init__(name, team, index, "./SeerV1/SeerV1_30000.pt")
